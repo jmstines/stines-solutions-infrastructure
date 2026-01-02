ca172697c98e32365df259a2a8f4dbe46d336210
Jeffrey Stines <jmstines00@gmail.com>
2026-01-02 11:38:24 -0500

    feat: Add lifecycle configuration for S3 bucket to manage old Lambda artifacts

--- modules/artifacts/main.tf
+++ modules/artifacts/main.tf
@@ -6,3 +6,20 @@
 resource "aws_s3_bucket" "lambda_artifacts" {
     bucket = "stines-solutions-lambda-artifacts"
 }
+
+resource "aws_s3_bucket_lifecycle_configuration" "lambda_artifacts" {
+  bucket = aws_s3_bucket.lambda_artifacts.id
+
+  rule {
+    id     = "delete-old-lambdas"
+    status = "Enabled"
+
+    filter {
+      prefix = "lambda/contact/"
+    }
+
+    expiration {
+      days = 7
+    }
+  }
+}
